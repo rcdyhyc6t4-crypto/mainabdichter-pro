bacb1a8a64e7c50264dcf34eff49cca5c2bc93c2
rcdyhyc6t4-crypto <rcdyhyc6t4@privaterelay.appleid.com>
2026-07-24 18:28:43 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ import { createWorksitePdf, createVisitPdf, downloadBlob } from "./pdf.js?v=30.0
 import { addWorksiteAttachment, listWorksiteAttachments, updateWorksiteAttachment, deleteWorksiteAttachment, safeAttachmentFilename } from "./attachments-v227.js";
 
 
-const MAINABDICHTER_APP_VERSION = "30.0.0";
+const MAINABDICHTER_APP_VERSION = "30.1.0";
 window.MAINABDICHTER_APP_VERSION = MAINABDICHTER_APP_VERSION;
 const MAINABDICHTER_WORKER_URL = "https://mainabdichter-api.cmww7htry5.workers.dev";
 
