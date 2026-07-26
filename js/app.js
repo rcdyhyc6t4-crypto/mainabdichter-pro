f01f4fa9bdb187f40125bccfe1aab795762865ff
rcdyhyc6t4-crypto <248410583+rcdyhyc6t4-crypto@users.noreply.github.com>
2026-07-26 12:00:09 +0200

    update

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ import { stageVisitDocument, syncPendingVisitDocuments, deleteQueuedVisitDocumen
 import { stageWorksitePhoto, deleteWorksitePhoto, hydrateWorksitePhotoImages, syncWorksitePhotos, migrateEmbeddedWorksitePhotos } from "./worksite-photos.js?v=32.7.8";
 
 
-const MAINABDICHTER_APP_VERSION = "32.8.0";
+const MAINABDICHTER_APP_VERSION = "32.8.1";
 window.MAINABDICHTER_APP_VERSION = MAINABDICHTER_APP_VERSION;
 const MAINABDICHTER_WORKER_URL = "https://mainabdichter-api.cmww7htry5.workers.dev";
 
@@ -800,6 +800,7 @@ function initializeV28Dashboard() {
   };
 
   if ($("v284AcceptedOffersCard")) $("v284AcceptedOffersCard").onclick = openAcceptedOffers;
+  if ($("v28OpenOffersCard")) $("v28OpenOffersCard").onclick = openAcceptedOffers;
   if ($("v28Notifications")) $("v28Notifications").onclick = openAcceptedOffers;
   if ($("v284CloseAcceptedOffers")) $("v284CloseAcceptedOffers").onclick = closeAcceptedOffers;
   if ($("v284AcceptedOffersModal")) {
